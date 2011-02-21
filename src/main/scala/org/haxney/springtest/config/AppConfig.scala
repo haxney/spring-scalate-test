1757b6f9254f0563ea2499143445bf8a94bf562e
Dan Hackney <dan@haxney.org>
2011-02-21 01:37:16 -0500

    Finally get <mvc:default-servlet-handler/> working
    Now properly serves both static and dynamic pages.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- src/main/scala/org/haxney/springtest/config/AppConfig.scala
+++ src/main/scala/org/haxney/springtest/config/AppConfig.scala
@@ -3,15 +3,27 @@ package config
 
 import org.springframework.context.annotation.{Bean, Configuration, ImportResource}
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.servlet.mvc.annotation.{DefaultAnnotationHandlerMapping, AnnotationMethodHandlerAdapter}
 import org.slf4j.LoggerFactory
 
 @Configuration
+@ImportResource(Array("classpath:/mvc-resources.xml"))
 class AppConfig {
 
   val logger = LoggerFactory.getLogger(classOf[AppConfig])
 
   @Bean
   def homeController = new Home
+
+  @Bean
+  def handlerMapping = {
+    val mapping = new DefaultAnnotationHandlerMapping
+    mapping.setOrder(0)
+    mapping
+  }
+
+  @Bean
+  def handlerAdapter = new AnnotationMethodHandlerAdapter
 
   @Bean
   def scalateView = {
