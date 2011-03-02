2bd9b3fae6e8b5e6e23bd8d6f9f31232b298f0bd
Dan Hackney <dan@haxney.org>
2011-03-02 00:18:39 -0500

    Project now compiles and runs with Neo4j config
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -20,17 +20,28 @@ import org.fusesource.scalate.sbt._
 
 class Project(info: ProjectInfo) extends DefaultWebProject(info) with PrecompilerWebProject {
 
-  lazy val fusesource_snapshot_repo = "FuseSource Snapshots" at
-           "http://repo.fusesource.com/nexus/content/repositories/snapshots"
-  lazy val java_net_repo = "Java.net Repository" at
-           "http://download.java.net/maven/2"
+  lazy val fusesource_snapshot_repo = "FuseSource Snapshots" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
+  lazy val java_net_repo = "Java.net Repository" at "http://download.java.net/maven/2"
   lazy val springMilestone = "Spring Framework Milestone Repository" at "http://maven.springframework.org/milestone"
+  lazy val jbossRepository   = "jboss" at "https://repository.jboss.org/nexus/content/groups/public"
+  lazy val neo_repo     = "Neo4j Maven Repository" at "http://m2.neo4j.org"
 
-  lazy val scalateSpring = "org.fusesource.scalate" % "scalate-spring-mvc" % "1.5.0-SNAPSHOT"
-  lazy val servlet       = "javax.servlet"          % "servlet-api"        % "2.5"
-  lazy val logback       = "ch.qos.logback"         % "logback-classic"    % "0.9.26"
-  lazy val cglib         = "cglib"                  % "cglib"              % "2.2"
+  lazy val scalateSpring = "org.fusesource.scalate"   % "scalate-spring-mvc" % "1.5.0-SNAPSHOT"
+  lazy val servlet       = "javax.servlet"            % "servlet-api"        % "2.5"
+  lazy val logback       = "ch.qos.logback"           % "logback-classic"    % "0.9.26"
+  lazy val cglib         = "cglib"                    % "cglib"              % "2.2"
   // to get jetty-run working in sbt
-  lazy val jetty_webapp  = "org.eclipse.jetty"      % "jetty-webapp"       % "7.0.2.RC0" % "test"
-  lazy val spring        = "org.springframework"    % "spring-webmvc"      % "3.1.0.M1"
+  lazy val jetty_webapp  = "org.eclipse.jetty"        % "jetty-webapp"       % "7.0.2.RC0" % "test"
+  lazy val spring        = "org.springframework"      % "spring-webmvc"      % "3.1.0.M1"
+  lazy val neo           = "org.neo4j"                % "neo4j-kernel"       % "1.3-SNAPSHOT"
+  lazy val data_graph    = "org.springframework.data" % "spring-data-neo4j"  % "1.0.0.M3"
+  lazy val aspectj       = "org.aspectj"              % "aspectjrt"          % "1.6.11.M2"
+  lazy val slf4j         = "org.slf4j"                % "jcl-over-slf4j"     % "1.6.1"
+  lazy val commons_log   = "commons-logging"          % "commons-logging"    % "1.1.1"     % "provided"
+
+  override def ivyXML =
+    <dependencies>
+      <exclude org="com.sun.jdmk"/>
+      <exclude org="com.sun.jmx"/>
+    </dependencies>
 }
--- src/main/scala/org/haxney/springtest/config/AppConfig.scala
+++ src/main/scala/org/haxney/springtest/config/AppConfig.scala
@@ -2,14 +2,16 @@ package org.haxney.springtest
 package config
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.{Bean, Configuration, Import, Feature, FeatureConfiguration, ComponentScanSpec}
+import org.springframework.context.annotation.{Bean, Configuration, ImportResource, Feature, FeatureConfiguration, ComponentScanSpec}
 import org.springframework.core.`type`.filter.AnnotationTypeFilter
+import org.springframework.data.graph.neo4j.config.Neo4jConfiguration
 import org.springframework.web.servlet.mvc.annotation.{DefaultAnnotationHandlerMapping, AnnotationMethodHandlerAdapter}
 import org.springframework.web.servlet.config.MvcDefaultServletHandler
 import org.slf4j.LoggerFactory
+import org.neo4j.kernel.EmbeddedGraphDatabase
 
 @Configuration
-class AppConfig {
+class AppConfig extends Neo4jConfiguration {
 
   val logger = LoggerFactory.getLogger(classOf[AppConfig])
 
@@ -30,6 +32,11 @@ class AppConfig {
     res.setSuffix(".jade")
     res
   }
+
+  lazy val storeDir = "target/neo-store"
+
+  @Bean(destroyMethod = "shutdown")
+  def graphDb = new EmbeddedGraphDatabase(storeDir)
 }
 
 @FeatureConfiguration
